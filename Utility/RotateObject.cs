2e35a1c039e21683ee84b89d5cf2c2eef0ce0dac
Torhque <Torhque@live.com>
2017-02-14 22:29:40 -0700

    Add files via upload

---
+++ Utility/RotateObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+	public int spinSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, spinSpeed * Time.deltaTime, 0);
+	}
+}
---
+++ Utility/Tag.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tag : MonoBehaviour {
+
+	public static Tag instance;
+
+	public bool isWaypoint; // Tag for waypoint objects
+	public bool isSpawnPoint;
+	public bool isPlayer; // Tag for player objects
+	public bool isPlayerOne;
+	public bool isPlayerTwo;
+	public bool isAITank;
+	public bool isFirepowerPU;
+	public bool isHealthPU;
+	public bool isFireRatePU;
+
+	void Awake () {
+
+
+	}
+}
